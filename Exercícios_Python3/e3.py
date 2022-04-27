b4c63efcb6f124fd09cf38d6b11f0975a9da7e6c
sofismavaz <sofisma.vaz@hotmail.com>
2022-04-27 18:10:13 -0500

    ajuste

--- Exercícios_Python3/e3.py
+++ Exercícios_Python3/e3.py
@@ -3,10 +3,11 @@ import os
 cam = '/home/lrocio/Projetos'
 
 def mostraDir(xargs):
+    print()
     
     for l1, l2, l3 in os.walk(cam):
-        #print(f'{l1} Campo1, {l2} Campo2, {l3} Campo3')
-        #print(f'{l1}')
+        print(f'{l1} Campo1, {l2} Campo2, {l3} Campo3')
+        print(f'{l1}')
         i = l1
         print(i)
 
