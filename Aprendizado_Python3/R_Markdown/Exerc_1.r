6a05ecb597558027adce55d4eba491adabca8bc0
sofismavaz <sofisma.vaz@hotmail.com>
2022-05-05 22:06:28 -0500

    estudo em casa

--- Aprendizado_Python3/R_Markdown/Exerc_1.r
+++ Aprendizado_Python3/R_Markdown/Exerc_1.r
@@ -46,12 +46,94 @@ array(A, dim = c(2,6,4))
 
 array(A, dim = c(2,6))
 
-import(cellranger)
 
-, cli, curl, gargle, cola, googledrive, httr, ids, magrittr, métodos, purrr , rematch2 , rlang, tibble, utils, vctrs)
+x <- rnorm(30)
+hist(x)
+
+library(rmarkdown)
+
+hgd(x)
+
+hgd_browse(x)
 
 
+x = seq(0, 133 * pi, by = 0.1)
+plot(x, sin(x), type = "l")
 
-url <- "https://docs.google.com/spreadsheets/d/1tAeMng_wLlZfDfeeLzgV8h7vGqWmFhqQv1AAXf60HtU/edit?usp=sharing"
+plot(x, cos(x), type = "l")
 
-df <- googlesheets4::read_sheet(url, sheet="sensib-fungic")
+
+library(ggplot2)
+
+###### Criação de Dataframes
+
+## Opção 1
+fornecedores<-c("A","B","E","D","X")
+quant_Jan<-c(320,230,100,340,10)
+quant_Fev<-c(220,630,60,50,60)
+quant_Mar<-c(520,430,100,34,10)
+
+resumo<-cbind(fornecedores,quant_Jan,quant_Fev,quant_Mar)
+resumo_l<-rbind(fornecedores,quant_Jan,quant_Fev,quant_Mar)
+
+r1 <- rbind(quant_Jan)
+
+r11 <- as.character(quant_Jan)
+r12 <- as.numeric(quant_Jan)
+
+## Opção 2
+resumo_df<-as.data.frame(resumo)
+
+dados <- data.frame(fornecedores=c("A","B","E","D","X"),
+            quant_Jan=c(320,230,100,340,10),
+           quant_Fev=c(220,630,60,50,60),
+           quant_Mar=c(520,430,100,34,10))
+
+## Opção 3
+# dados<-data.frame(V1=fornecedores,V2=quant_Jan,V3=quant_Fev)
+
+
+nrow(resumo)
+
+for (i in 1:nrow(dados)) {
+ print()
+}
+
+str(r12)
+
+str(r1)
+
+dados$quant_Fev
+
+library(knitr)
+
+## Dimensionamento do data frame
+
+ncol(dados)
+nrow(dados)
+
+dim(dados)
+names(dados)
+
+utils::View(dados)
+utils::View(dados$quant_Fev)
+
+summary(dados)
+
+
+a<-c(1,3,5,7,9)
+b<-c(5,6,3,8,9)
+c<-c("a","a","b","a","b")
+
+dados_1<-data.frame(a,b,c)
+
+utils::View(dados_1)
+summary(dados_1)
+
+dados_1[c(1:5),c(2,3)]
+dados_1[,2]
+
+setwd("/home/lrocio/Projetos")
+
+s1 <- (2*4)+(3**2)/(4.1-1.3)
+
